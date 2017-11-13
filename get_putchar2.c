a379820e9c81172d63ea107f8a20148d499fd624
Max King <maksimko@starport.com.ua>
2017-11-13 12:11:12 +0200

    Commit all c-sorce files from c_learn

---
+++ get_putchar2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// copy input to output; v.2
+
+main()
+{
+    int c;
+    
+//    c = getchar();
+    while ((c = getchar()) != EOF) {
+	putchar (c);
+//	c = getchar();
+    }
+}
+
