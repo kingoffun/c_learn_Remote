61d34d2822c8cc0d0900b0c64ba3fdfc2e86a21e
Max King <maksimko@starport.com.ua>
2017-11-22 17:50:52 +0200

    task 1-19 v.3

--- 1-19reversLine.c
+++ 1-19reversLine.c
@@ -4,18 +4,21 @@
 
 void getLine();
 void reversLine(char ln[]);
+void freeLine();
 
 char line[MAXLINE];
 char rline[MAXLINE];
+int c,notMAX;
 
 main()
 {
-    int c;
-
     while((c = getchar()) != EOF){
 	getLine();
+//	printf("%s\n", line);
 	reversLine(line);
-	printf("%s\n", rline);
+	printf("%s", rline);
+//	putchar(rline[MAXLINE-1]);
+	freeLine();
     }
 	
     return 0;
@@ -26,13 +29,17 @@ void reversLine(char ln[])
     //char s[MAXLINE];
     int i,j;
     
-    if(ln[MAXLINE-2] == '\n'){
+    if(notMAX == 0){
     
-	for (i = MAXLINE-3, j = 0; i >= 0; --i, ++j)
+	for (i = MAXLINE-2, j = 0; i >= 0; --i, ++j)
 	    rline[j] = ln[i];
 	
 	rline[j] = ln[j];
-	++j;
+    }
+    else if(notMAX < MAXLINE-1) {
+	for (i = notMAX-2, j = 0; i >= 0; --i, ++j)
+	    rline[j] = ln[i];
+	
 	rline[j] = ln[j];
     }
 }
@@ -40,15 +47,27 @@ void reversLine(char ln[])
 // getline: read line into s, return length
 void getLine()
 {
-    int c, i;
+    int i;
     
-    for (i = 0; i < MAXLINE-1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    for (i = 0; i < MAXLINE-1 && c != '\n'; ++i){
 	line[i] = c;
-
-    if (c == '\n'){
-	line[i] = c;
-	++i;
+	c = getchar();
     }
-    
-    line[i] = '\0';
+    line[i] = '\n';
+
+    if (i < MAXLINE-1)
+	notMAX = i;
+    else if (i == MAXLINE-1)
+	notMAX = 0;
+}
+
+void freeLine()
+{
+    int i;
+
+    for (i = 0; i < MAXLINE; ++i)
+	line[i] = 0;
+
+    for (i = 0; i < MAXLINE; ++i)
+	rline[i] = 0;
 }
