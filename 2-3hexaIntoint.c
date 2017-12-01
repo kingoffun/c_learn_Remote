ded4afe04eadd150e218b0575924a204e72a8270
Max King <maksimko@starport.com.ua>
2017-12-01 22:01:49 +0200

    task 2-3 remaked from site

--- 2-3hexaIntoint.c
+++ 2-3hexaIntoint.c
@@ -1,28 +1,41 @@
 #include <stdio.h>
 
-#define LINE 5
+#define IN 1
+#define OUT 0
 
-int htol(char hexa[LINE]);
+int htol(char hexa[]);
 
 int main()
 {
-    int c, i;
-    char s[LINE];
-    
-    while((c = getchar()) != EOF){
-	for(i = 0; i < LINE && (c = getchar()) != EOF && c != '\n';++i){
-	    s[i]=c;
-	}
-
-	printf("line = %s, hex = %d\n", s, htol(s));
-}
+    char s[4] = {'0','x','F','f'};
+    printf("%d\n", htol(s));
 
     return 0;
 }
 
 int htol(char s[])
 {
-    int i = 0;
-    i = (int)s;
-    return i;
+    int i = 0, number = 0, indigit = IN, hexdig;
+    
+    if(s[i] == '0'){
+	i++;
+	if(s[i] == 'x' || 'X')
+	    i++;
+    }
+
+    for(;indigit == IN;i++){
+	if(s[i] >= '0' && s[i] <= '9')
+	    hexdig = s[i] - '0';
+	else if(s[i] >= 'a' && s[i] <= 'f')
+	    hexdig = s[i] - 'a' + 10;
+	else if(s[i] >= 'A' && s[i] <= 'F')
+	    hexdig = s[i] - 'A' + 10;
+	else
+	    indigit = OUT;
+
+	if(indigit == IN)
+	    number = number*16 + hexdig;
+    }
+
+    return number;
 }
