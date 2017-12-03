29698ea25a17ba599b243d7fac1966e298b7f6f2
eeePc <eeePc@home.com>
2017-12-04 00:20:49 +0200

    task 2-4 eeePC

--- 2-4deletes2froms1.c
+++ 2-4deletes2froms1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX 10
 
@@ -6,24 +7,42 @@ void squeeze(char s1[], char s2[]);
 
 int main()
 {
-    char toFind[6] = {'a','b','c','1','2','3'};
+    char toFind[] = "abc123";
     char line[MAX];
     int i, c;
 
+    //while((c = getchar()) != EOF){
     for(i = 0;i < MAX-1 && (c = getchar()) != EOF && c != '\n';i++){
 	    line[i] = c;
+	    //c = getchar();
     }
 
-    printf("%d\n", htol(s));
+    if(i == MAX-1 && c != '\n')
+	line[i-1] = '\n';
 
+    if(i < MAX-1)
+	line[i++] = '\n';
+
+    line[i] = '\0';
+
+    //printf("%s\n",toFind);
+    squeeze(line,toFind);
+    printf("%s", line);
+    //}
     return 0;
 }
 
-void squeeze(char s1[], s2[])
+void squeeze(char s1[], char s2[])
 {
-    int i, c;
-    
-    
-
-    return number;
+    int i, j;
+    //printf("%d\n",strlen(s2));
+    for(i = 0; s1[i] != '\n';i++){
+	for(j = strlen(s2) - 1; j >= 0; j--)
+	    //printf("j - %d", j/);
+	    if(s1[i] == s2[j]){
+		//printf("i - %c j - %c\n", s1[i], s2[j]);
+		s1[i] = '|';
+		break;
+	    }
+    }
 }
