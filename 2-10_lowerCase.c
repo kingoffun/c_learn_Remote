c0a28de0abbe2ed60e65cfe85cd5b829f42f5baa
Max King <maksimko@starport.com.ua>
2017-12-12 17:32:07 +0200

    task 2-10 from site

--- 2-10_lowerCase.c
+++ 2-10_lowerCase.c
@@ -8,12 +8,11 @@ int main(){
     int c;
 
     while((c = getchar()) != EOF)
-	printf("%c", lower(c));
+	putchar(lower(c));
 
     return 1;
 }
 
 int lower(int c){
-    
-    return (c >= 65 && c <= 90) ? c+=32 : c;
+    return (c >= 'A' && c <= 'Z') ? c += 'a'-'A' : c;
 }
